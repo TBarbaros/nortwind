c566ce1961703e4633d8687f90749daa7da2a4a0
TBarbaros <trkdsun@gmail.com>
2021-03-14 15:10:25 +0300

    push

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/nortwind'),
+      dir: require('path').join(__dirname, './coverage/northwind'),
       subdir: '.',
       reporters: [
         { type: 'html' },
